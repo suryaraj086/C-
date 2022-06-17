bbc6fcc49f819e10eec2e0ff28ab65cfde940577
suryaraj086 <79685586+suryaraj086@users.noreply.github.com>
2022-06-17 10:27:58 +0530

    Add files via upload

--- Task5/main.cpp
+++ Task5/main.cpp
@@ -16,21 +16,32 @@ namespace first_space
             return val * val * val;
         }
     }
-    int findMax(int val1, int val2)
+
+    inline namespace third_space
     {
-        if (val1 > val2)
+        int findMax(int val1, int val2)
         {
-            return val1;
+            if (val1 > val2)
+            {
+                return val1;
+            }
+            return val2;
         }
-        return val2;
     }
-    int findMin(int val1, int val2)
+
+    namespace minimum
     {
-        if (val1 < val2)
+        namespace minVal
         {
-            return val1;
+            int findMin(int val1, int val2)
+            {
+                if (val1 < val2)
+                {
+                    return val1;
+                }
+                return val2;
+            }
         }
-        return val2;
     }
 }
 
@@ -47,52 +58,52 @@ int main()
     cin >> val;
     switch (val)
     {
-    case 1:
-    {
-        cout << t.calculatePi();
-        cout << maths::Math::pi();
-        break;
-    }
-    case 2:
-    {
-        cout << "Enter the value";
-        int num;
-        cin >> num;
-        square(num);
-        break;
-    }
-    case 3:
-    {
-        cout << "Enter the value";
-        int num;
-        cin >> num;
-        cout << "Enter the second value";
-        int num1;
-        cin >> num1;
-        int out = findMin(num, num1);
-        cout << "the minimum value is " << out;
-        break;
-    }
-    case 4:
-    {
-        cout << "Enter the value";
-        int num;
-        cin >> num;
-        cout << "Enter the second value";
-        int num1;
-        cin >> num1;
-        int out = findMax(num, num1);
-        cout << "the maximum value is " << out;
-        break;
-    }
-    case 5:
-    {
-        cout << "Enter the value";
-        int num;
-        cin >> num;
-        second_space::cube(num);
-        break;
-    }
+        case 1:
+            {
+                cout << t.calculatePi();
+                cout << maths::Math::pi();
+                break;
+            }
+        case 2:
+            {
+                cout << "Enter the value";
+                int num;
+                cin >> num;
+                square(num);
+                break;
+            }
+        case 3:
+            {
+                cout << "Enter the value";
+                int num;
+                cin >> num;
+                cout << "Enter the second value";
+                int num1;
+                cin >> num1;
+                int out = minimum::minVal::findMin(num, num1);
+                cout << "the minimum value is " << out;
+                break;
+            }
+        case 4:
+            {
+                cout << "Enter the value";
+                int num;
+                cin >> num;
+                cout << "Enter the second value";
+                int num1;
+                cin >> num1;
+                int out = findMax(num, num1);
+                cout << "the maximum value is " << out;
+                break;
+            }
+        case 5:
+            {
+                cout << "Enter the value";
+                int num;
+                cin >> num;
+                second_space::cube(num);
+                break;
+            }
     }
     return 0;
 }
