8bb7c24cf43208d5f5a4da8308db78670b053e08
suryaraj086 <79685586+suryaraj086@users.noreply.github.com>
2022-07-02 16:58:25 +0530

    Add files via upload

--- hyperloop/src/hyperloop/Hyperloop.java
+++ hyperloop/src/hyperloop/Hyperloop.java
@@ -1,5 +1,44 @@
 package hyperloop;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+class SortByAge implements Comparator<Passenger> {
+
+	public int compare(Passenger a, Passenger b) {
+
+		return a.getAge() - b.getAge();
+	}
+}
+
 public class Hyperloop {
+
+	List<Passenger> passengerList = new LinkedList<>();
+
+	public void addPassenger(Passenger pass) {
+		passengerList.add(pass);
+	}
+
+	public void sortList() {
+		Collections.sort(passengerList, new SortByAge());
+	}
+
+	public Passenger getHighAgePassenger() {
+		int index = passengerList.size() - 1;
+		Passenger pass = passengerList.get(index);
+		passengerList.remove(index);
+		return pass;
+	}
+
+	public String passengerQueue() {
+		String out = "";
+		int size = passengerList.size();
+		for (int i = 0; i < size; i++) {
+			out += passengerList.get(i).toString();
+		}
+		return out;
+	}
 
 }
--- hyperloop/src/hyperloop/Runner.java
+++ hyperloop/src/hyperloop/Runner.java
@@ -3,34 +3,84 @@ package hyperloop;
 import java.util.Scanner;
 
 public class Runner {
+	static NodeWeighted A = new NodeWeighted(0, "A");
+	static NodeWeighted B = new NodeWeighted(1, "B");
+	static NodeWeighted C = new NodeWeighted(2, "C");
+	static NodeWeighted D = new NodeWeighted(3, "D");
+	static NodeWeighted E = new NodeWeighted(4, "E");
+	static NodeWeighted F = new NodeWeighted(5, "F");
+	static NodeWeighted G = new NodeWeighted(6, "G");
+	static GraphWeighted graphWeighted = new GraphWeighted(true);
+
+	public static void addSourceAndDestination(String source, String destination, int distance) {
+		graphWeighted.addEdge(getObject(source), getObject(destination), distance);
+	}
+
+	public static NodeWeighted getObject(String source) {
+		switch (source) {
+		case "A":
+			return A;
+		case "B":
+			return B;
+		case "C":
+			return C;
+		case "D":
+			return D;
+		case "E":
+			return E;
+		case "F":
+			return F;
+		default:
+			break;
+		}
+		return null;
+	}
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Hyperloop hyper = new Hyperloop();
 		boolean bool = true;
-		char Spoint;
+//		graphWeighted.addEdge(A, C, 7);
+//		graphWeighted.addEdge(B, D, 2);
+//		graphWeighted.addEdge(B, C, 2);
+//		graphWeighted.addEdge(A, B, 3);
+//		graphWeighted.addEdge(B, E, 5);
+//		graphWeighted.addEdge(C, E, 1);
+//		graphWeighted.addEdge(D, E, 3);
+//		String out = graphWeighted.DijkstraShortestPath(A, C);
+//		System.out.println(out);
+		String Spoint = null;
 		while (bool) {
+			System.out.println("Enter the command");
 			String inp = scan.nextLine();
 			if (inp.contains("INIT")) {
-				Spoint = inp.charAt(inp.length() - 1);
-				int totalStation = inp.charAt(inp.length() - 3);
+				Spoint = inp.charAt(inp.length() - 1) + "";
+				int totalStation = Integer.parseInt(inp.charAt(inp.length() - 3) + "");
 				for (int i = 0; i < totalStation; i++) {
-
+					String val = scan.nextLine();
+					String[] arr = val.split(" ");
+					addSourceAndDestination(arr[0], arr[1], Integer.parseInt(arr[2]));
 				}
 			} else if (inp.contains("ADD_PASSENGER")) {
-				int length = inp.charAt(inp.length() - 1);
+				int length = Integer.parseInt(inp.charAt(inp.length() - 1) + "");
 				for (int i = 0; i < length; i++) {
 					String data = scan.nextLine();
 					String[] arr = data.split(" ");
-
+					Passenger pass = new Passenger(arr[0], Integer.parseInt(arr[1]), arr[2]);
+					hyper.addPassenger(pass);
 				}
+				System.out.println("Added");
+				hyper.sortList();
 			} else if (inp.contains("START_POD")) {
-				int members = inp.charAt(inp.length() - 1);
+				int members = Integer.parseInt(inp.charAt(inp.length() - 1) + "");
 				for (int i = 0; i < members; i++) {
-
+					Passenger pass = hyper.getHighAgePassenger();
+					String path = graphWeighted.DijkstraShortestPath(getObject(Spoint),
+							getObject(pass.getDestination()));
+					System.out.println(pass.toString() + path);
 				}
 			} else if (inp.contains("PRINT_Q")) {
-
+				System.out.println(hyper.passengerQueue());
 			}
 		}
 		scan.close();
